3b64886438ea6b5272fdeebc0840b386b547e77a
Edenilson Amaya <20200004@ricaldone.edu.sv>
2024-06-08 21:03:25 -0600

    CRUD y Log in

---
+++ Soporte.sql
@@ -0,0 +1,16 @@
+Create Table tbTickets(
+uuid varchar2(50),
+Titulo varchar2(50),
+Descripcion varchar2(100),
+Autor varchar2(50),
+Email varchar2(50),
+FechaCreacion varchar2(50),
+Estado varchar2(25),
+FechaFinalizacion varchar2(50)
+);
+
+Create Table tbUsuarios(
+UUID_usuario VARCHAR2(50),
+correoElectronico VARCHAR2(50),
+clave VARCHAR2(50)
+);
